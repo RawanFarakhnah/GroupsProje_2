ed331b8fdc7185768ab6135284cdb23b002b6c02
Rawan Farakhnah <rawanali1997@yahoo.com>
2020-05-16 16:56:29 +0300

    // project recyclerView & CardView complete

---
+++ app/src/main/java/com/rawanali97/projectgroup2/MainActivity.java
@@ -0,0 +1,90 @@
+package com.rawanali97.projectgroup2;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.rawanali97.projectgroup2.model.Hotel;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //test comment
+        RecyclerView recycler = (RecyclerView)findViewById(R.id.hottel_recycler);
+        String[] captions = new String[Hotel.hotel.length];
+        int[] ids = new int[Hotel.hotel.length];
+
+        for(int i = 0; i<captions.length;i++){
+            captions[i] = Hotel.hotel[i].getName();
+            ids[i] = Hotel.hotel[i].getImg_id();
+        }
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(captions, ids);
+        recycler.setAdapter(adapter);
+       //       addDefaultData();
+      //        saveData();
+    }
+
+    public void addDefaultData(){
+        hotelList.add( new Hotel("Hilton Pyramids Golf",R.drawable.hilton_pyramids_golf));
+        hotelList.add( new Hotel("Makkah Hotel", R.drawable.makkah_hotel));
+        hotelList.add( new Hotel("Hotel Hilton Alexandria",R.drawable.hotel_hilton_alexandria));
+        hotelList.add(new Hotel("Oriental Rivoli Hotel",R.drawable.oriental_rivoli_hotel));
+        hotelList.add(new Hotel("Raffles Makkah Palace",R.drawable.raffles_makkah_palace));
+        hotelList.add(new Hotel( "The Westin Cairo Gol", R.drawable.the_westin_cairo_gol));
+        hotelList.add(new Hotel( "Sonesta Cairo Hotel", R.drawable.sonesta_cairo_hotel));
+        hotelList.add(new Hotel( "Sharm El Sheikh Marriott Resort",R.drawable.sharm_el_sheikh_marriott_resort));
+    }
+
+    private void saveData(){
+        loadData();
+        //hotelList.add( new Hotel("Hilton Pyramids Golf",R.drawable.hilton_pyramids_golf));
+        hotelList.add(new Hotel( "helloer",1));
+        SharedPreferences s = getSharedPreferences("HOTELS", MODE_PRIVATE);
+        SharedPreferences.Editor editor = s.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(hotelList);
+        editor.putString("hotellist", json);
+        editor.apply();
+
+    }
+
+
+    private void loadData() {
+        SharedPreferences s = getSharedPreferences("HOTELS", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = s.getString("hotellist", null);
+        Type type = new TypeToken<ArrayList<Hotel>>() {
+        }.getType();
+        if (gson.fromJson(json, type) != null) {
+            ArrayList<Hotel> load = gson.fromJson(json, type);
+            if (load != null && load.isEmpty() != true) {
+                hotelList = load;
+
+                print(load.toString());
+
+            }
+        }
+
+    }
+
+    private void print(String msg) {
+        Toast.makeText(getApplicationContext(), msg,
+                Toast.LENGTH_LONG).show();
+    }
+
+
+}
